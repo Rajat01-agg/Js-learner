97482b8e1ce9c0e237216bc7fdd1e85f3cdb8f82
Rajat01-agg <rajataggarwal20820@gmail.com>
2025-04-12 13:54:26 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,37 @@
+"use strict"
+
+/* Initial versions of js doesn,t have classes, modules and many other things
+ thus when js versions get updated too much of code is already written to avoid 
+ wastage of that code , js make a thing called "use strict" 
+ it implies all things coded now will use newer version policies */
+
+//alert(3+3) //we are using nodejs ,not browser.
+
+//Coder should focus on Code readability.
+
+/*Different types of datatypes in javaScript*/
+
+/* PRIMITIVE DATA TYPES */
+// number => 
+// bigInt =>
+// String => "any sequence of characters"
+// boolean => true/false
+// null => 
+// undefined => 
+// symbol =>
+
+
+// object
+
+
+//typeof => It returns datatype of given value or variable
+
+let age = 15
+console.log(typeof age)
+
+console.log(typeof "123")
+
+console.log(typeof null)   // come object
+console.log(typeof undefined)  // come undefined
+
+
