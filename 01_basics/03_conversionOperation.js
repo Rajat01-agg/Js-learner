7059c4263b2d2619967bf302e3b5805852abeacf
Rajat01-agg <rajataggarwal20820@gmail.com>
2025-04-14 06:39:02 +0000

    container config done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,38 @@
+let score 
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let ValueInNumber = Number(score)
+// console.log(typeof ValueInNumber)
+
+// console.log(ValueInNumber)
+
+
+// During printing typeof datatype using typeof Command answer comes in small but
+// during conversion we use capital name of datatype in our code
+// 
+// "33" => 33
+// "33abc" => NaN
+// "abc" => NaN
+// null => 0
+// undefined => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = "ab"
+// console.log(isLoggedIn)
+let booleanisLoggedIn = Boolean(isLoggedIn)
+
+// console.log(typeof booleanisLoggedIn)
+// console.log(booleanisLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "abc" => true
+
+let someNumber = 33;
+let someNumbertoString = String(someNumber)
+
+console.log(typeof someNumbertoString)
+console.log(someNumbertoString)
